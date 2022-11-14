91aec3c8ca40bf958c1e26b140cb4fb384290161
aganoa-vehikl <aganoa@vehikl.com>
2022-11-14 16:41:59 -0500

    added base models

---
+++ database/migrations/2022_11_14_212813_create_talks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('talks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('url');
+            $table->unsignedSmallInteger('length');
+            $table->foreignId('conference_id')->constrained();
+            $table->foreignId('speaker_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('talks');
+    }
+};
