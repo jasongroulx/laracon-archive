29e7c1c20344e7b5253097dc724d3eecf786064b
aganoa-vehikl <aganoa@vehikl.com>
2022-11-14 16:48:33 -0500

    add base relations to models

--- app/Models/Talk.php
+++ app/Models/Talk.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Talk extends Model
 {
     use HasFactory;
+
+    public function conference(): BelongsTo
+    {
+        return $this->belongsTo(Conference::class);
+    }
+
+    public function speaker(): BelongsTo
+    {
+        return $this->belongsTo(Speaker::class);
+    }
 }
