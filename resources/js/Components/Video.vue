62487ccc794677010228d3929d83c50895bc900e
aganoa-vehikl <aganoa@vehikl.com>
2022-12-20 16:34:36 -0500

    Add animations

--- resources/js/Components/Video.vue
+++ resources/js/Components/Video.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative group overflow-hidden hover:scale-110 transition-all">
+    <div class="relative group overflow-hidden hover:scale-110 transition-all duration-300">
         <img class="object-cover" :src="video.thumbnailUrl" :alt="video.title">
         <div class="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-50 group-hover:opacity-80"></div>
         <div class="text-white absolute bottom-0 p-5">
@@ -13,7 +13,7 @@
 
         </div>
 
-        <div class="hidden group-hover:flex absolute top-0 bottom-0 left-0 right-0 items-center justify-center">
+        <div class="opacity-0 group-hover:opacity-100 duration-300 transition-all flex absolute top-0 bottom-0 left-0 right-0 items-center justify-center">
             <Link :href="`/watch/${video.id}`" class="rounded-full bg-red-600 text-white p-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
